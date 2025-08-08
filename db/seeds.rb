c605730151a2fb642ca77c6b5762d86dadc4d67b
lyuhiroyama <lyuhiroyama@gmail.com>
2025-08-08 15:32:02 +0900

    db: Seed flights (For every origin-destination airport pair)
    - Excludes same airport pairs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
-["HND", "NRT", "CTS", "YYZ", "YVR"].each do |code|
+[ "HND", "NRT", "CTS", "YYZ", "YVR" ].each do |code|
   Airport.find_or_create_by!(airport_code: code)
+end
+
+airports = Airport.all.to_a
+
+airports.each do |origin|
+  airports.each do |destination|
+    next if origin == destination
+
+    Flight.create!(
+      origin_id: origin.id,
+      destination_id: destination.id,
+      departure_time: DateTime.now + rand(1..10).days + rand(0..23).hours,
+      duration_minutes: rand(60..600)
+    )
+  end
 end
