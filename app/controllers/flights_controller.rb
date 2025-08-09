031b244b63c6968c70bd0a3885918c9c54546bba
lyuhiroyama <lyuhiroyama@gmail.com>
2025-08-10 02:20:19 +0900

    feat: Add flight selection with radio buttons
    - Add search result detection with @search_performed
    
    - Add flight selection form with radio buttons in view
    
    - Include hidden field to pass num_passengers to booking controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,12 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @search_performed = params.except(:controller, :action).present?
+
+    if @search_performed
+      @flights = @flights.where(origin_id: params[:origin_id]) if params[:origin_id].present?
+      @flights = @flights.where(destination_id: params[:destination_id]) if params[:destination_id].present?
+      @flights = @flights.where("DATE(departure_time) = ?", params[:departure_time]) if params[:departure_time].present?
+    end
   end
 end
