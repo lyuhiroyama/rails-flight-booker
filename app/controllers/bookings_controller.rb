a72e237d257973444f7d36333a6ee667b41a5262
lyuhiroyama <lyuhiroyama@gmail.com>
2025-08-11 00:03:15 +0900

    feat: Implement booking creation (with passenger handling)
    - Add 'create' action to BookingsController
    
    - Set strong parameters
    
    - Implement booking save, with automatic passenger creation via nested attributes
    
    - Add hidden flight_id field to booking form in views/bookings/new.html.erb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,4 +8,22 @@ class BookingsController < ApplicationController
       @booking.passengers.build
     end
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    # -> Also replaces empty passenger objects from #new with submitted passenger attributes
+    if @booking.save
+      redirect_to @booking, notice: "Booking successfully created"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.expect(
+      booking: [ :flight_id, passengers_attributes: [ [ :name, :email ] ]
+  ])
+  end
 end
