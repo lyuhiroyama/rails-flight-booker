f4ca9b8c74dcaa59c391ed35994d810c3b7c9585
lyuhiroyama <lyuhiroyama@gmail.com>
2025-08-10 20:25:57 +0900

    fix: Resolve Rubocop linting violations (spacing and whitespace)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,11 @@
 class BookingsController < ApplicationController
-  def new 
+  def new
     @flight = Flight.find(params[:flight_id])
     @num_passengers = params[:num_passengers].to_i
-
     @booking = Booking.new(flight: @flight)
-
     # Blank Passenger objects for the form
     @num_passengers.times do
       @booking.passengers.build
     end
-  end
-
-  def create 
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "flights#index"
-  resources :flights, only: [:index]
-  resources :bookings, only: [:new, :create, :show]
+  resources :flights, only: [ :index ]
+  resources :bookings, only: [ :new, :create, :show ]
 end
